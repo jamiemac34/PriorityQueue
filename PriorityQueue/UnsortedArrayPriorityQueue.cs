2b06be08c3596a37c29571c33987f21486833aaa
Jamie Maclennan <22000375@uhi.ac.uk>
2025-03-22 02:16:25 +0000

    Added unsorted array add & remove, although visual output is incorrect

--- PriorityQueue/UnsortedArrayPriorityQueue.cs
+++ PriorityQueue/UnsortedArrayPriorityQueue.cs
@@ -11,34 +11,91 @@ namespace PriorityQueue
     {
         private readonly PriorityItem<T>[] storage;
         private readonly int capacity;
-        private int tailIndex;
+        private int count;
 
         public UnsortedArrayPriorityQueue(int size)
         {
             storage = new PriorityItem<T>[size];
             capacity = size;
-            tailIndex = -1;
-        }
-
-        public void Add(T item, int priority)
-        {
-            throw new NotImplementedException();
+            count = -1;
         }
 
         public T Head()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException();
+            }
+            return storage[0].Item;
         }
 
-        public bool IsEmpty()
+        public void Add(T item, int priority)
         {
-            throw new NotImplementedException();
+            count++;
+            if (count >= capacity)
+            {
+                count--;
+                throw new QueueOverflowException();
+            }
+
+            storage[count] = new PriorityItem<T>(item, priority);
         }
 
         public void Remove()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException();
+            }
+
+            if (count - 1 != -1)
+            {
+                storage[getHighestPriortiy()] = storage[count - 1];
+            }
+            
+            count--;
+        }
+
+        public bool IsEmpty()
+        {
+            return count < 0;
+        }
+
+        public override string ToString()
+        {
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException("No items to display");
+            }
+
+            string result = "[";
+            for (int i = 0; i <= count; i++)
+            {
+                if (i > 0)
+                {
+                    result += ", ";
+                }
+                result += storage[i];
+            }
+            result += "]";
+            return result;
+        }
+
+        //function to find the highest priority by looping through the array
+        private int getHighestPriortiy()
+        {
+               int index = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (storage[i].Priority > storage[index].Priority)
+                {
+                    index = i;
+                }
+
+            }
+
+            return index;
         }
     }
 }
-
