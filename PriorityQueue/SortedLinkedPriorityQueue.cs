5a21bf5f1c5dd22c401e89e9bb4dd923dcb38680
Jamie Maclennan <22000375@uhi.ac.uk>
2025-03-22 14:18:40 +0000

    Sorted linked list implemented

--- PriorityQueue/SortedLinkedPriorityQueue.cs
+++ PriorityQueue/SortedLinkedPriorityQueue.cs
@@ -9,36 +9,91 @@ namespace PriorityQueue
 
     public class SortedLinkedPriorityQueue<T> : PriorityQueue<T>
     {
-        private readonly PriorityItem<T>[] storage;
-        private readonly int capacity;
-        private int tailIndex;
+
+        private class Node
+        {
+            public PriorityItem<T> Entry;
+            public Node Next;
+            public Node(PriorityItem<T> Entry)
+            {
+                this.Entry = Entry;
+                this.Next = null;
+            }
+
+        }
+
+        private Node head;
+
 
         public SortedLinkedPriorityQueue(int size)
         {
-            storage = new PriorityItem<T>[size];
-            capacity = size;
-            tailIndex = -1;
-        }
-
-        public void Add(T item, int priority)
-        {
-            throw new NotImplementedException();
+            head = null;
         }
 
         public T Head()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException();
+            }
+            return head.Entry.Item;
+        }
+
+        public void Add(T item, int priority)
+        {
+            Node newEnt = new Node(new PriorityItem<T>(item, priority));
+            if (head == null || head.Entry.Priority < newEnt.Entry.Priority) {
+                newEnt.Next = head;
+                head = newEnt; }
+            else
+            {
+                Node current = head;
+                while (current.Next != null && current.Next.Entry.Priority > newEnt.Entry.Priority) 
+                {
+                    current = current.Next;
+                }
+                newEnt.Next = current.Next;
+                current.Next = newEnt;
+            }
+        }
+
+
+
+
+        public void Remove()
+        {
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException();
+            }
+
+            head = head.Next;
+
         }
 
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
+            return head == null;
         }
 
-        public void Remove()
+        public override string ToString()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException("No items to display");
+            }
+
+            string result = "[";
+            Node current = head;
+            while (current != null)
+            {
+                result += current.Entry.ToString();
+                current = current.Next;
+            }
+
+            result += "]";
+            return result;
         }
+
     }
 }
-
