0c917e0ac67f5a7a354e16204d2bfafb54d96d8b
Jamie Maclennan <22000375@uhi.ac.uk>
2025-03-22 16:12:51 +0000

    Heap implemented

--- PriorityQueue/HeapPriorityQueue.cs
+++ PriorityQueue/HeapPriorityQueue.cs
@@ -3,41 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PriorityQueue
 {
 
     public class HeapPriorityQueue<T> : PriorityQueue<T>
     {
-        private readonly PriorityItem<T>[] storage;
-        private readonly int capacity;
-        private int tailIndex;
+        private readonly List<PriorityItem<T>> storage;
 
         public HeapPriorityQueue(int size)
         {
-            storage = new PriorityItem<T>[size];
-            capacity = size;
-            tailIndex = -1;
+            storage = new List<PriorityItem<T>>(size);
         }
 
         public void Add(T item, int priority)
         {
-            throw new NotImplementedException();
+            storage.Add(new PriorityItem<T>(item, priority));
+            int index = storage.Count - 1;
+
+            while (index > 0) 
+            { 
+                int parent = (index - 1) / 2;
+                if (storage[index].Priority <= storage[parent].Priority)
+                {
+                    break;
+                }
+                Swap(index, parent);
+                index = parent;
+            }
         }
 
         public T Head()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException();
+            }
+            return storage[0].Item;
         }
 
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
+            return storage.Count() == 0;
         }
 
         public void Remove()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException();
+            }
+
+            storage[0] = storage[storage.Count - 1];
+            storage.RemoveAt(storage.Count - 1);
+
+            int index = 0;
+            int priorIndex = storage.Count - 1;
+            while (true)
+            {
+                int lc = 2 * index + 1;
+                int rc = 2 * index + 2;
+                int largest = index;
+
+                if (lc <= priorIndex && storage[lc].Priority > storage[largest].Priority)
+                {
+                    largest = lc;
+                }
+                if (rc <= priorIndex && storage[rc].Priority > storage[largest].Priority)
+                {
+                    largest = rc;
+                }
+                if (largest == index)
+                {
+                    break;
+                }
+                Swap(index, largest);
+                index = largest;
+            }
+        }
+
+        public override string ToString()
+        {
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException("No items to display");
+            }
+
+            return "[" + String.Join(", ", storage) + "]";
+        }
+
+        private void Swap(int i, int j)
+        {
+            var temp = storage[i];
+            storage[i] = storage[j];
+            storage[j] = temp;
         }
     }
 }
